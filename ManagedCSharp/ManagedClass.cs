e3161a0f95af94eb1d3858703efbef14f8c30382
Ranjit Pati <ranjit.pati@siemens.com>
2022-05-20 12:55:05 +0530

    initial commit

---
+++ ManagedCSharp/ManagedClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ManagedCSharp
+{
+    public static class ManagedClass
+    {
+        public static void ShowValue(ref int value)
+        {
+            Console.WriteLine("Test print");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
